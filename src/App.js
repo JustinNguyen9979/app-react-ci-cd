05e15247ce06503296b910e1fbc366f9e17caaf0
JustinNguyen9979 <duchuy011097@gmail.com>
2024-02-05 05:47:02 +0700

    update 1.5.2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          OK CI-CD SUCCESS Version: 1.5.1
+          OK CI-CD SUCCESS Version: 1.5.2
         </p>
         <a
           className="App-link"
