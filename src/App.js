3e8f23c4e145bfa507b6dd4a568702f904fcc54e
JustinNguyen9979 <duchuy011097@gmail.com>
2024-02-05 05:25:54 +0700

    test push ver 1.5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          OK CI-CD SUCCESS
+          OK CI-CD SUCCESS Version: 1.5
         </p>
         <a
           className="App-link"
